420cfc2e4eeed0ad5cd6a709c8d4100a6c2cb318
royce <royce.george.pro@outlook.fr>
2020-11-14 17:16:39 +0100

    add fixtures user and Account

--- src/DataFixtures/AccountFixtures.php
+++ src/DataFixtures/AccountFixtures.php
@@ -2,16 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Account;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\DataFixtures\UserFixtures;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class AccountFixtures extends Fixture
+class AccountFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i=0; $i < 3 ; $i++) {
+          $account = new Account();
+          $account->setAccountType('Compte courant n° '. $i);
+          $account->setOpeningDate(new \DateTime);
+          $account->setBalance($i);
+          //On récupère la référence dans l'entité user pour lié les deux fixtures entre elles
+          $account->setUser($this->getReference(UserFixtures::TOTO_USER_REFERENCE));
+          $manager->persist($account);
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            UserFixtures::class,
+        );
     }
 }
