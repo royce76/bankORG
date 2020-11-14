31be08f32808b5d518a5a44f1b6389180c2fc87c
royce <royce.george.pro@outlook.fr>
2020-11-14 21:20:57 +0100

    add 2 opérations for 3 acccounts in fixtures

--- src/DataFixtures/AccountFixtures.php
+++ src/DataFixtures/AccountFixtures.php
@@ -10,24 +10,23 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class AccountFixtures extends Fixture implements DependentFixtureInterface
 {
-    public const TOTO_ACCOUNT_REFERENCE = 'toto_account';
+    public const TOTO_ACCOUNT_REFERENCE = ['1','2','3'];
 
     public function load(ObjectManager $manager)
     {
-        for ($i=0; $i < 3 ; $i++) {
+        foreach (self::TOTO_ACCOUNT_REFERENCE as $value) {
           $account = new Account();
-          $account->setAccountType('Compte courant n° '. $i);
+          $account->setAccountType('Compte courant n° '. $value);
           $account->setOpeningDate(new \DateTime);
-          $account->setBalance($i + 100);
+          $account->setBalance(100);
           //On récupère la référence dans l'entité user pour lié les deux fixtures entre elles
           $account->setUser($this->getReference(UserFixtures::TOTO_USER_REFERENCE));
+          // other fixtures can get this object using the UserFixtures::ADMIN_USER_REFERENCE constant
+          $this->addReference($value, $account);
           $manager->persist($account);
         }
 
         $manager->flush();
-
-        // other fixtures can get this object using the UserFixtures::ADMIN_USER_REFERENCE constant
-        $this->addReference(self::TOTO_ACCOUNT_REFERENCE, $account);
 
     }
 
--- src/DataFixtures/OperationFixtures.php
+++ src/DataFixtures/OperationFixtures.php
@@ -13,17 +13,19 @@ class OperationFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        for ($i=0; $i < 3 ; $i++) {
-          $operation = new Operation();
-          $operation->setOperationType('Opération n° '. $i);
-          $operation->setAmount($i + 20);
-          $operation->setComments('Justificatif n°' . $i);
-          $operation->setDateTransaction(new \DateTime('2020-11-'.$i));
+        foreach (AccountFixtures::TOTO_ACCOUNT_REFERENCE as $value) {
+          for ($i=0; $i < 2; $i++) {
+            $operation = new Operation();
+            $operation->setOperationType('Opération n° '. $i);
+            $operation->setAmount(20);
+            $operation->setComments('Justificatif n°' . $i);
+            $operation->setDateTransaction(new \DateTime);
 
-          //On récupère la référence dans l'entité user pour lié les deux fixtures entre elles
-          $operation->setUser($this->getReference(UserFixtures::TOTO_USER_REFERENCE));
-          $operation->setAccount($this->getReference(AccountFixtures::TOTO_ACCOUNT_REFERENCE));
-          $manager->persist($operation);
+            //On récupère la référence dans l'entité user pour lié les deux fixtures entre elles
+            $operation->setUser($this->getReference(UserFixtures::TOTO_USER_REFERENCE));
+            $operation->setAccount($this->getReference($value));
+            $manager->persist($operation);
+          }
         }
 
         $manager->flush();
