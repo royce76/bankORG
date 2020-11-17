5043d72b68e04def42456d1800887505a0e0f984
royce <royce.george.pro@outlook.fr>
2020-11-17 23:40:13 +0100

    add flash message, responsive  from reset twig to login with assert

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -121,7 +121,10 @@ class ResetPasswordController extends AbstractController
 
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
-
+            $this->addFlash(
+            'success',
+            'Bravo, essayez votre nouveau mot de passe!'
+            );
             return $this->redirectToRoute('app_login');
         }
 
